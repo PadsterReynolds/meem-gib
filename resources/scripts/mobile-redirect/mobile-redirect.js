10a9f87c7c743139396c075d9c9c1e6ee4900bc7
Scott Munn <scott.munn@e-smartz.com>
2018-04-13 16:07:35 +0400

    mobile-redirect test 11 CC

--- resources/scripts/mobile-redirect/mobile-redirect.js
+++ resources/scripts/mobile-redirect/mobile-redirect.js
@@ -5,9 +5,6 @@ $(function() {
 
     function mobileVsDesktop() {
         if (isMobileDevice() === true){
-//            $('.apple').toggle("slow");
-//            $('.android').toggle("slow");
-//        } else if (isMobileDevice() === true){
             customizeForDevice()
         }
     }
@@ -23,24 +20,13 @@ $(function() {
             android: ua.match(/Android/)
         };
         if (checker.android){
-//            $('.apple').toggle("slow");
-//            $('.apple-apply').toggle("slow");
-//            $('.login').toggle("slow");
-//            $('.apply').toggle("slow");
             $(".login[href='https://online.meem.bh']").attr('href', 'https://play.google.com/store/apps/details?id=com.gib.meembah')
             $(".apply[href='https://online.meem.bh']").attr('href', 'https://play.google.com/store/apps/details?id=com.gib.meembah')
         }
         else if (checker.iphone){
-//            $('.android').toggle("slow");
-//            $('.android-apply').toggle("slow");
-//            $('.login').toggle("slow");
-//            $('.apply').toggle("slow");
             $(".login[href='https://online.meem.bh']").attr('href', 'https://itunes.apple.com/us/app/meem-bah/id1345368637?mt=8')
             $(".apply[href='https://online.meem.bh']").attr('href', 'https://itunes.apple.com/us/app/meem-bah/id1345368637?mt=8')
         }
     }
-
-//    $(".login[href='http://www.google.com/']").attr('href', 'http://www.live.com/')
-
 
 });
