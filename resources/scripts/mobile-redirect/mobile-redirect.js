58ecc9592737d3cc370707b488a17df9642921a4
Scott Munn <scott.munn@e-smartz.com>
2018-04-13 13:49:26 +0400

    mobile-redirect testing in CC

--- resources/scripts/mobile-redirect/mobile-redirect.js
+++ resources/scripts/mobile-redirect/mobile-redirect.js
@@ -1,18 +1,32 @@
 $(function() {
 
-    mobileVsDesktop();
+//    mobileVsDesktop();
 
-    function isMobileDevice() {
-        return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
-    }
-
-    function mobileVsDesktop() {
-        if (isMobileDevice() === false){
-            $('.apple').toggle("slow");
-            $('.android').toggle("slow");
-        } else if (isMobileDevice() === true){
-            $('.login').toggle("slow");
-            $('.apply').toggle("slow");
+    var customizeForDevice = function(){
+        var ua = navigator.userAgent;
+        var checker = {
+            iphone: ua.match(/(iPhone|iPod|iPad)/),
+            android: ua.match(/Android/)
+        };
+        if (checker.android){
+            $('.android').append("hello");
+        }
+        else if (checker.iphone){
+            $('.iphone').append("hello");
         }
     }
-});
+    
+//    function isMobileDevice() {
+//        return (typeof window.orientation !== "undefined") || (navigator.userAgent.indexOf('IEMobile') !== -1);
+//    }
+
+//    function mobileVsDesktop() {
+//        if (isMobileDevice() === false){
+//            $('.apple').toggle("slow");
+//            $('.android').toggle("slow");
+//        } else if (isMobileDevice() === true){
+//            $('.login').toggle("slow");
+//            $('.apply').toggle("slow");
+//        }
+//    }
+//});
